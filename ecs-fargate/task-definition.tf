1070718377d90579bd3e074aa5a36e72030f8e65
Senna <ayrton.coelho@willbank.com.br>
2022-01-11 18:38:51 -0300

    WIP ajustando data types na task definition

--- ecs-fargate/task-definition.tf
+++ ecs-fargate/task-definition.tf
@@ -1,23 +1,17 @@
 data "aws_caller_identity" "this" {}
 data "aws_region" "this" {}
 
-resource "aws_ecs_task_definition" "this" {
-  family                   = var.service_name
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = var.network_mode
-  cpu                      = var.service_cpu
-  memory                   = var.service_memory
-  execution_role_arn       = var.iam_role
-  task_role_arn            = var.iam_role
+data "template_file" "this" {
+  template = file("${path.module}/documents/task-definitions/default_task_definition.json")
 
-  container_definitions = templatefile("${path.module}/documents/task-definitions/default_task_definition.json", {
+  vars = {
     #Configurações do serviço.
     SERVICE_NAME        = var.service_name
     SERVICE_MEMORY      = var.service_memory
     SERVICE_CPU         = var.service_cpu
     SERVICE_MOUNTPOINTS = jsonencode(var.service_mountpoints)
     SERVICE_ULIMITS     = jsonencode(var.service_ulimits)
-    SERVICE_ENTRYPOINTS = jsonencode(var.service_entrypoint)
+    SERVICE_ENTRYPOINT  = jsonencode(var.service_entrypoint)
     SERVICE_COMMAND     = jsonencode(var.service_command)
     SERVICE_ENVIRONMENT = jsonencode(var.service_environment)
     SERVICE_SECRETS     = jsonencode(var.service_secrets)
@@ -31,17 +25,33 @@ resource "aws_ecs_task_definition" "this" {
     HEALTHCHECK_START    = local.service_health_check.start
 
     #Configurações de rede do serviço.
-    AWS_REGION    = data.aws_region.this.name
-    NETWORK_MODE  = var.network_mode
-    PORT_MAP      = replace(jsonencode(local.port_mappings), "/\"([0-9]+\\.?[0-9]*)\"/", "$1",)
-  })
+    AWS_REGION   = data.aws_region.this.name
+    NETWORK_MODE = var.network_mode
+    PORT_MAP     = replace(jsonencode(local.port_mappings), "/\"([0-9]+\\.?[0-9]*)\"/", "$1",)
+  }
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = var.service_name
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = var.network_mode
+  cpu                      = var.service_cpu
+  memory                   = var.service_memory
+  execution_role_arn       = var.iam_role
+  task_role_arn            = var.iam_role
+
+  container_definitions = data.template_file.this.rendered
 
   depends_on = [aws_lb_target_group.this]
 }
 
 data "aws_ecs_task_definition" "this" {
   task_definition = aws_ecs_task_definition.this.family
-  depends_on      = [aws_ecs_task_definition.this, aws_ecr_repository.this]
+
+  depends_on = [
+    aws_ecs_task_definition.this, 
+    aws_ecr_repository.this
+  ]
 }
 
 
@@ -50,7 +60,7 @@ locals {
   service_health_check = {
     command  = var.service_health_check.command
     start    = var.service_health_check.start
-    timeout  = var.healthcheck_task_timeout != null ? var.healthcheck_task_timeout : local.service_health_check.timeout
+    timeout  = var.healthcheck_task_timeout != null ? var.healthcheck_task_timeout : var.service_health_check.timeout
     interval = var.service_health_check.interval
     retries  = var.service_health_check.retries
   }
