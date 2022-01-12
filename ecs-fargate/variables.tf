7f3e7de499bdf84005a039ff91dab8a77847d907
Senna <ayrton.coelho@willbank.com.br>
2022-01-11 21:48:26 -0300

    Criando as validacoes das variaveis

--- ecs-fargate/variables.tf
+++ ecs-fargate/variables.tf
@@ -1,11 +1,26 @@
 ##SERVICE VARS
 
 #Required
-variable "service_name" {}
+variable "service_name" {
+  validation {
+    condition     = can(regex("[[:alpha:]]", var.service_name)) == true
+    error_message = "[ERROR]::: A variável 'service_name' deve ser do tipo String."
+  }
+}
 
-variable "service_port" {}
+variable "service_port" {
+  validation {
+    condition     = can(regex("[[:digit:]]", var.service_port)) == true
+    error_message = "[ERROR]::: A variável 'service_port' deve ser do tipo Number."
+  }
+}
 
-variable "cluster_name" {}
+variable "cluster_name" {
+  validation {
+    condition     = can(regex("[[:alpha:]]", var.cluster_name)) == true
+    error_message = "[ERROR]::: A variável 'cluster_name' deve ser do tipo String."
+  }
+}
 
 variable "network_settings" {
   type = object({
@@ -17,17 +32,47 @@ variable "network_settings" {
 }
 
 #Optional
-variable "desired_count" { default = 1 }
+variable "desired_count" { 
+  default = 1 
+  validation {
+    condition     = can(regex("[[:digit:]]", var.desired_count)) == true
+    error_message = "[ERROR]::: A variável 'desired_count' deve ser do tipo Number."
+  }
+}
 
-variable "platform_version" { default = "1.4.0" }
+variable "platform_version" { 
+  default = "1.4.0"
+  validation {
+    condition     = var.platform_version == "1.4.0" || var.platform_version == "1.3.0"
+    error_message = "[ERROR]::: O valor inserido para 'platform_version' não é válido, deve ser '1.4.0'."
+  }
+}
 
-variable "tg_protocol" { default = "TCP" }
+variable "enable_lb" {
+  validation {
+    condition     = can(tobool(var.enable_lb)) == true
+    error_message = "[ERROR]::: A variável 'enable_lb' deve ser do tipo Boolean(true or false)."
+  }
 
-variable "enable_lb" { default = true }
+  default = true 
+}
 
-variable "enable_sd" { default = true }
+variable "enable_sd" { 
+  validation {
+    condition     = can(tobool(var.enable_sd)) == true
+    error_message = "[ERROR]::: A variável 'enable_sd' deve ser do tipo Boolean(true or false)."
+  }
 
-variable "healthcheck_grace_period" { default = 60 }
+  default = true 
+}
+
+variable "healthcheck_grace_period" { 
+  default = 60
+  validation {
+    condition     = can(regex("[[:digit:]]", var.healthcheck_grace_period)) == true
+    error_message = "[ERROR]::: A variável 'healthcheck_grace_period' deve ser do tipo Number."
+  }
+}
 
 variable "deployment_settings" {
   type = object({
